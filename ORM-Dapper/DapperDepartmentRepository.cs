d872400e53d5332a270db5e338ebbe56f43b0100
Tom Settle <tom.k.settle@gmail.com>
2023-02-28 12:54:24 -0600

    Successfully added a department!

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+        
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("select * from Departments;");
+        }
+
+        public void InsertDepartment(string newDeptName)
+        {
+            _connection.Execute("insert into Departments (Name) values (@deptName);", new { deptName = newDeptName });
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+            
+            IDbConnection conn = new MySqlConnection(connString);
+            var repo = new DapperDepartmentRepository(conn);
+
+            Console.Write("Enter a new department name: ");
+            var newDept = Console.ReadLine();
+            repo.InsertDepartment(newDept);
+
+            var depts = repo.GetAllDepartments();
+            foreach (var dept in depts)
+            {
+                Console.WriteLine($"{dept.Name} ({dept.DepartmentID})");
+            }
         }
     }
 }
